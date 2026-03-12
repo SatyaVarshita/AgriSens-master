db5a5fa4607324136f66ac7d1fb126e15bf4680b
SatyaVarshita <159930322+SatyaVarshita@users.noreply.github.com>
2026-03-12 19:37:44 +0530

    Update webapp.py

--- AgriSens-master/FERTILIZER-RECOMMENDATION/webapp.py
+++ AgriSens-master/FERTILIZER-RECOMMENDATION/webapp.py
@@ -16,6 +16,7 @@ warnings.filterwarnings('ignore')
 from PIL import Image
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+st.write("Files in this folder:", os.listdir(BASE_DIR))
 csv_path = os.path.join(BASE_DIR, "Fertilizer_recommendation.csv")
 
 if os.path.exists("fertilizer.png"):
